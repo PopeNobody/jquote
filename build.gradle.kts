53c8af14434ab9e966580cc5062a29e2e8676879
nobody <github2@invisiblehand.church>
2023-01-30 21:20:58 -0500

    x

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("net.sf.cssbox:cssbox:1.0.0")
+    implementation("net.sf.cssbox:swingbox:1.1")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+        Document document = Jsoup.connect("http://invisiblehand.church").get();
+        System.out.println(document);
     }
 }
